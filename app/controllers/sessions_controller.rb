4e412f64eaa2fc40930aba710a49d54b4d8e615a
Loren Peterson <lorenkp@gmail.com>
2015-06-09 13:56:58 -0700

    static pages controller created--routing to backbone unless logged out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+    if @user.nil?
+      @user = User.new
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    else
+      log_in(@user)
+      render text: 'logged in'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def destroy
+    log_out
+    redirect_to new_session_url
+  end
+end
