0f59671697140ae65c486ffab8b525c17d0594dd
Loren Peterson <lorenkp@gmail.com>
2015-06-09 13:33:43 -0700

    basic authentication and sign up working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return nil unless session[:token]
+    @current_user ||= User.find_by_session_token(session[:token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def log_in(user)
+    user.reset_session_token
+    session[:session_token] = user.session_token
+  end
+
+  def log_out
+    current_user.reset_session_token
+    session[:session_token] = nil
+  end
+
+  def require_signed_in!
+    redirect_to new_session_url unless logged_in?
+  end
 end
