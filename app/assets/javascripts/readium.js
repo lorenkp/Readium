0dc3d602923caf14849a12d72c75fc56f39cf5c1
Loren Peterson <lorenkp@gmail.com>
2015-06-09 20:40:52 -0700

    a little formating of sign in page

---
+++ app/assets/javascripts/readium.js
@@ -0,0 +1,13 @@
+window.Readium = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    // alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Readium.initialize();
+});
