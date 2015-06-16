fb3b1d254becfcad4b6053222e33e4818fab7d04
Loren Peterson <lorenkp@gmail.com>
2015-06-15 20:58:08 -0700

    migrated following

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612222840) do
+ActiveRecord::Schema.define(version: 20150616035536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followable_id"
+    t.string   "followable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "follows", ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id", using: :btree
 
   create_table "stories", force: :cascade do |t|
     t.integer  "author_id",  null: false
