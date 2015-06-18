2914abd8f9ba1f3eb84573dbd5547f48e6f887b8
Loren Peterson <lorenkp@gmail.com>
2015-06-17 23:28:20 -0700

    backbone followed users and tags associations appear to be working

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,31 @@
 Readium.Models.User = Backbone.Model.extend({
-  urlRoot: 'api/stories'
+  urlRoot: 'users',
+
+  followedTags: function() {
+    if (!this._followedTags) {
+      this._followedTags = new Readium.Collections.Tags([], {user: this});
+    }
+    return this._followedTags;
+  },
+
+
+ followedUsers: function() {
+    if (!this._followedUsers) {
+      this._followedUsers = new Readium.Collections.Users([], {user: this});
+    }
+    return this._followedUsers;
+  },
+
+  parse: function(response) {
+    if (response.followed_users) {
+      this.followedUsers().set(response.followed_users);
+      delete response.followed_users;
+    }
+
+    if (response.followed_tags) {
+      this.followedTags().set(response.followed_tags);
+      delete response.followed_tags;
+    }
+    return response;
+  }
 });
