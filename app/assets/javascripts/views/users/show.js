1980dc6b7ab74249d419da8b0037438253fe67ec
Loren Peterson <lorenkp@gmail.com>
2015-06-22 19:56:25 -0700

    promising progress on user page formating

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -1,8 +1,14 @@
 Readium.Views.UserShow = Backbone.CompositeView.extend({
   template: JST['users/show'],
 
+  initialize: function() {
+    this.listenTo(this.model, 'sync', this.render);
+  },
+
   render: function() {
-    var content = this.template();
+    var content = this.template({
+      user: this.model
+    });
     this.$el.html(content);
     return this;
   }
--- app/views/api/stories/index.json.jbuilder
+++ app/views/api/stories/index.json.jbuilder
@@ -1,5 +1,6 @@
 json.array! @stories do |story|
   json.extract! story, :title, :body, :subtitle, :id, :header_url, :home_url
   json.username story.author.username
+  json.author_id story.author.id
   json.created_at story.created_at.iso8601
 end
