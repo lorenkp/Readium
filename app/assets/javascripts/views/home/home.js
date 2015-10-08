e0d0f607f10be4e2e7d98114ae9a8e110aaada5a
Loren Peterson <lorenkp@gmail.com>
2015-10-07 20:44:54 -0700

    removed defunct listentos and addstory functions in home view

--- app/assets/javascripts/views/home/home.js
+++ app/assets/javascripts/views/home/home.js
@@ -7,12 +7,6 @@ Readium.Views.Home = Backbone.CompositeView.extend({
     this.storiesCollection = options.storiesCollection;
     this.tagsCollection = options.tagsCollection;
 
-    // this.storiesCollection.each(this.addStoryFeedPreview.bind(this));
-
-    // this.listenTo(this.storiesCollection, 'add remove sync', this.render);
-
-    // this.listenTo(this.storiesCollection, 'add', this.addNewStoryFeedPreview);
-
     this.addSubview('.compose', new Readium.Views.ComposeHome({
       storiesCollection: this.storiesCollection,
       tagsCollection: this.tagsCollection
@@ -27,20 +21,6 @@ Readium.Views.Home = Backbone.CompositeView.extend({
     this.addSubview('.story-feed', new Readium.Views.StoryFeed({
       collection: this.storiesCollection
     }));
-  },
-
-  addNewStoryFeedPreview: function(story) {
-    var view = new Readium.Views.StoryFeedPreview({
-      model: story
-    });
-    this.addSubview('.story-feed', view, true);
-  },
-
-  addStoryFeedPreview: function(story) {
-    var view = new Readium.Views.StoryFeedPreview({
-      model: story
-    });
-    this.addSubview('.story-feed', view);
   },
 
   render: function() {
